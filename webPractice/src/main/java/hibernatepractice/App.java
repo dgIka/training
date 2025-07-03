a6774beb92b5a72130e1e61a0a5462e0cd715990
Ika <rexar.ika@gmail.com>
2025-07-04 00:50:50 +0300

    girst task with hibernate

--- webPractice/src/main/java/hibernatepractice/App.java
+++ webPractice/src/main/java/hibernatepractice/App.java
@@ -5,12 +5,15 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
-        Configuration cfg = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
+        Configuration cfg = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class)
+                .addAnnotatedClass(Director.class)
+                .addAnnotatedClass(Movie.class);
 
         SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.getCurrentSession();
@@ -38,8 +41,29 @@ public class App {
 //            List<Item> items = person.getItems();
 //            items.stream().forEach(a -> session.remove(a));
 //            person.getItems().clear();
+//            Director director = session.get(Director.class, 1);
+//            System.out.println(director.getName());
+//            director.getMovies().forEach(System.out::println);
+//            System.out.println("------------------------");
+//            Movie movie = session.get(Movie.class, 8);
+//            System.out.println(movie.getName());
+//            System.out.println(movie.getDirector().getName());
 
+//            Director director1 = session.get(Director.class, 3);
+//            Movie movie1 = new Movie("Master", director1, 2025);
+//            director1.getMovies().add(movie1);
+//            session.save(movie1);
+//            Director director2 = session.get(Director.class, 8);
+//            Movie movie2 = new Movie("Lord of the rings", director2, 2002);
+//            session.update(director2);
+//            session.save(movie2);
 
+            Movie movie = session.get(Movie.class, 14);
+            Director director = movie.getDirector();
+            director.setMovies(Collections.singletonList(null));
+            movie.setDirector(session.get(Director.class, 4));
+
+            session.update(movie);
 
             session.getTransaction().commit();
 
---
+++ webPractice/src/main/java/hibernatepractice/model/Movie.java
@@ -0,0 +1,69 @@
+package hibernatepractice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @Column(name = "movie_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "director_id", referencedColumnName = "director_id")
+    private Director director;
+
+    @Column(name = "year_of_production")
+    private int year;
+
+    public Movie() {
+    }
+
+    public Movie(String name, Director director, int year) {
+        this.name = name;
+        this.director = director;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.director + " " + this.year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
