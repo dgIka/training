2d494b2848c3cffdc4237df948cf4d1e2420bd04
Ika <rexar.ika@gmail.com>
2025-08-17 00:27:24 +0300

    hibernate чтото там

---
+++ jrHibernateTraining/src/main/java/entity/Person.java
@@ -0,0 +1,52 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "second_name")
+    private String second_name;
+
+    @Column(name = "birth_date")
+    private LocalDate birth_date;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getSecond_name() {
+        return second_name;
+    }
+
+    public void setSecond_name(String second_name) {
+        this.second_name = second_name;
+    }
+
+    public LocalDate getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(LocalDate birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
